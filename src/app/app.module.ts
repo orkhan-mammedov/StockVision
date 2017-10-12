f465a466895f899e1f4ad1b6d240c12fefda9236
Orkhan Mammedov <orkhan.elshan@outlook.com>
2017-10-11 22:15:24 -0400

    - Create basic components (no logic added) - Add basic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,40 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { RouterModule, Routes, RouterOutlet } from '@angular/router';
 import { MaterialModule } from '../material/material.module';
+import { NewsComponent } from './news/news.component';
+import { StocksComponent } from './stocks/stocks.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'news',
+    component: NewsComponent },
+  { path: 'stocks',
+    component: StocksComponent },
+  { path: '',
+    redirectTo: '/news',
+    pathMatch: 'full' },
+  { path: '**',
+    component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewsComponent,
+    StocksComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    RouterModule,
     MaterialModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
